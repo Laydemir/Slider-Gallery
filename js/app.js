c447e3578f587630c486b60e771d914ee6b3485a
Laydemir <voldvakoff@gmail.com>
2022-05-13 22:58:18 +0300

    разместили фотограцфии

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 const sliderMain = new Swiper('.slider--main', {
-
+	freeMode: true, //перемотка слайда без привязки к крайней позиции
+	centeredSlides: true, //первый слайд находится в центре
+	mousewheel: true,//прокрутка мышью
+	parallax: true, //паралакс
+	breakpoints: { //слайды начинаются в центре экрана
+		0: { //от 0px до 680px
+			slidesPerView: 2.5,
+			spaceBetween: 20 //расстояние между слайдами
+		},
+		680: {// От 680px до бесконечности
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
 })
